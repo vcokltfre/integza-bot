434ccf27b364265a2b41672669e12af151d0bdb5
Marco <marconovino@gmail.com>
2021-04-02 03:21:34 -0300

    255

--- Int3gza.py
+++ Int3gza.py
@@ -87,7 +87,6 @@ async def on_message(message):
     if("@" in message.content):
         if(message.mentions[0].id == 414918675481493506):
             await message.channel.send("||<@275291687637745665> <@" + str(message.author.id) +">||", embed = pingembed)
-    #ping embed
     if(message.content == ">ping"):
         pingembed = discord.Embed(title="Ping", color=0x0c0f27)
         pingembed.add_field(name="Bot", value=f'🏓 Pong! {round(bot.latency * 1000)}ms')
@@ -101,91 +100,6 @@ async def on_reaction_add(message, reaction, user):
     if user != bot.user:
         if str(reaction.emoji) == "🗑️":
             await my_last_message.delete(my_last_message)
-
-
-#ping
-async def ping(ctx):
-  embed = discord.Embed(title="Ping", color=0x0c0f27)
-  embed.add_field(name="Bot", value=f'🏓 Pong! {round(bot.latency * 1000)}ms')
-  embed.set_footer(text=f"Request by {ctx.author}", icon_url=ctx.author.avatar_url)
-  await ctx.send(embed=embed)
-
-#ping
-
-
-#rules
-
-@bot.command()
-async def printrules(ctx):
-    embed = discord.Embed(
-        title="Rules", description="", color=0x0c0f27)
-    embed.add_field(
-        name="Prusa MK3S", value="The Prusa is a great printer overall, it is quite expensive but worth it. Great option if you arent on a budget and works amazing out of the box \n \n **Price: U$D 750** \n \n", inline=False)
-    embed.add_field(
-        name="Creality Ender 3", value="The ender 3 is a great printer overall, a great Prusa clone. It is a mid range printer that can get up to Prusa performance with some tweaking\n \n **Price: U$D 200** \n \n", inline=False)
-    embed.add_field(
-        name="FLSUN QQ-S Pro", value="The QQ-S is a great delta printer at an amazing price, the big brother of the Q5\n \n **Price: U$D 364** \n \n", inline=False)
-    embed.add_field(
-        name="Elegoo Mars", value="The mars is a great sla printer for a good price, resin printers take time to use, so if you are looking for a prototyping machine that can output models fast get an FDM printer\n \n **Price: U$D 190**", inline=False)
-    await ctx.send(embed=embed)
-
-#FAQ
-
-#clear
-
-
-@bot.command()
-@commands.has_permissions(manage_messages=True)
-async def clear(ctx, amount: int):
-    await ctx.channel.purge(limit=amount + 1)
-    deletemessage = await ctx.send(f"{amount} messages got deleted.")
-    await asyncio.sleep(3)
-    await deletemessage.delete()
-
-#clear
-
-#kick
-
-
-@bot.command()
-@commands.has_permissions(kick_members=True)
-async def kick(ctx, member: discord.Member, *, reason=None):
-    await member.kick(reason=reason)
-    await ctx.send(f'Kicked {member.mention}\nReason: {reason}')
-
-#kick
-
-#ban
-
-
-@bot.command()
-@commands.has_permissions(ban_members=True)
-async def ban(ctx, member: discord.Member, *, reason=None):
-    await member.ban(reason=reason)
-    await ctx.send(f'Banned {member.mention}\nReason: {reason}')
-
-#ban
-
-#unban
-
-
-@bot.command()
-@commands.has_permissions(ban_members=True)
-async def unban(ctx, *, member):
-    banned_users = await ctx.guild.bans()
-    member_name, member_discriminator = member.split('#')
-
-    for ban_entry in banned_users:
-        user = ban_entry.user
-
-        if (user.name, user.discriminator) == (member_name, member_discriminator):
-            await ctx.guild.unban(user)
-            await ctx.send(f'Unbanned {user.mention}')
-            return
-
-#unban
-
-#background tasks
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -206,7 +120,5 @@ async def on_command_error(ctx, error):
                               colour=0xe73c24)
         await ctx.send(embed=embed)
         raise error
-
-#background tasks
 
 bot.run(TOKEN)
