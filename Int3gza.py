7a0917245bcc494970c587a4f363be09213fcfdc
Marco <marconovino@gmail.com>
2021-04-02 03:42:54 -0300

    3

--- Int3gza.py
+++ Int3gza.py
@@ -37,12 +37,6 @@ printembed.add_field(
 intpingembed = discord.Embed(
     title="Dont ping integza please!", description="Please read rule 10, if you ping integza there is a lower chance he will see it!", color=0x0c0f27)
 
-# Ping Embed #
-
-pingembed = discord.Embed(title="Ping", color=0x0c0f27)
-pingembed.add_field(name="Bot", value=f'🏓 Pong! {round(bot.latency * 1000)}ms')
-pingembed.set_footer(text=f"Request by {message.author}", icon_url=message.author.avatar_url)
-
 #  Start  #
 
 @bot.event
@@ -105,11 +99,14 @@ async def on_message(message):
     
     printsend = 0
     metalsend = 0
-    
+
     if("@" in message.content):
         if(message.mentions[0].id == 414918675481493506):
             await message.channel.send("||<@275291687637745665> <@" + str(message.author.id) +">||", embed = intpingembed)
     if(message.content == ">ping"):
+        pingembed = discord.Embed(title="Ping", color=0x0c0f27)
+        pingembed.add_field(name="Bot", value=f'🏓 Pong! {round(bot.latency * 1000)}ms')
+        pingembed.set_footer(text=f"Request by {message.author}", icon_url=message.author.avatar_url)
         await message.channel.send(embed=pingembed)
 
         
